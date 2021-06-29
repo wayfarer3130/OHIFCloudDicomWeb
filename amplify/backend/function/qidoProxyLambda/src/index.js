0f437ac7ced69a85a23936921c0b25ea2d3ec0c8
Bill Wallace <wayfarer3130@gmail.com>
2021-06-29 17:24:08 -0400

    Added a qido lambda function

---
+++ amplify/backend/function/qidoProxyLambda/src/index.js
@@ -0,0 +1,15 @@
+
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('QIDO from Lambda!'),
+    };
+    return response;
+};
