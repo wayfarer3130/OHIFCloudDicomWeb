d9f2585b7fa1abfd0efbcec1e4d89e6b1daabde2
Bill Wallace <wayfarer3130@gmail.com>
2021-06-29 09:46:35 -0400

    Added backend "staging"

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
